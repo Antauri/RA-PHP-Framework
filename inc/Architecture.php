0194493a9141f6f1f8e95d0f503fca6fe16f7b43
Zamfir Catalin Alexandru <catalin.zamfir@kitsoftware.ro>
2019-03-26 21:40:08 +0200

    Update Architecture.php

--- inc/Architecture.php
+++ inc/Architecture.php
@@ -1,4 +1,8 @@
 <?php
+/**
+    Edited for prof. Catalin Apostolescu, UTM, an I, ID.
+*/
+
 /*
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
